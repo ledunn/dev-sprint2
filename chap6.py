194ec79ee5f4ad35a384e39379d859dc5d54855d
ledunn <laurenelizabeth.dunn@gmail.com>
2014-05-29 16:53:38 -0400

    chap6

--- chap6.py
+++ chap6.py
@@ -1,9 +1,43 @@
-# Enter your answrs for chapter 6 here
-# Name:
+# Enter your answers for chapter 6 here
+# Name: Lauren Dunn
 
 
 # Ex. 6.6
 
+def first(word):
+	return word[0]
+
+def last(word):
+	return word[-1]
+
+def middle(word):
+	return word[1:-1]
+
+print middle('')
+
+#middle() doesn't work with two letter, one, or no letter strings.
+
+def is_palindrome(word):
+	if len(word)<2:
+		return "False"
+
+	if first(word)!= last(word):
+		print 'False'
+
+	else: print'True'
+
+	return is_palindrome(word[-1])
 
 
-# Ex 6.8
+print is_palindrome('racecar')
+
+
+# Ex 6.8:
+
+def is_power(a,b):
+	if a%b and (a%b)%b
+		print "Yes"
+		else: print "No"	
+
+print is_power(2,4)
+
--- chap7.py
+++ chap7.py
@@ -1,9 +1,17 @@
-# Enter your answrs for chapter 7 here
-# Name:
+# Enter your answers for chapter 7 here
+# Name: Lauren Dunn
 
 
 # Ex. 7.5
+def estimate)pi:
+	while True:
+
+		if 
+
+		break
+
 
 
 
 # How many iterations does it take to converge?
+
